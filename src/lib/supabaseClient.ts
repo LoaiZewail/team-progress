2c7fc3ec3b1b6ad7f36e8f3d33dadaa4805b26c7
Loai Zewail <loaizewail@gmail.com>
2025-02-06 19:11:07 +0200

    Fix : hot fix

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    "https://ymkxczbcoikqrmpfhirv.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlta3hjemJjb2lrcXJtcGZoaXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NjAxNTUsImV4cCI6MjA1NDQzNjE1NX0.Twitr0nzcs6jh2XFZe2AzGobJvZIv8b58JoYK80OB8c"
 );
